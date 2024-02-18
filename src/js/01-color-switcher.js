fcef4ae37de488cf8883f77c58ee896307f77da3
Aleksandermo <143647602+Aleksandermo@users.noreply.github.com>
2024-02-18 19:36:51 +0100

    feat: init6 jshw9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,21 +1,22 @@
+let timerId;
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-function handleClick(action) {
-  if (action === "Start") {
+const handleClick = (action) => {
+    if (action === "Start") {
     startButton.disabled = true;
-    timerId = setTimeout(function changeColor() {
-      document.body.style.backgroundColor = getRandomHexColor();
-      timerId = setTimeout(changeColor, 1000);
-    }, 1000);
-  } else if (action === "Stop") {
-    startButton.disabled = false;
-    clearTimeout(timerId);
-  }
-}
+    timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    }
+    else if (action === "Stop") {
+        startButton.disabled = false;
+        clearInterval(timerId);
+    }
+};
 /* */
 startButton.addEventListener("click", () => handleClick("Start"));
 stopButton.addEventListener("click", () => handleClick("Stop")); 
