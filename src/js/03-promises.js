65359772efdd245e3586880e4ebcf94554bdbb35
Aleksandermo <143647602+Aleksandermo@users.noreply.github.com>
2024-02-18 23:14:55 +0100

    feat: init11 jshw9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import Notiflix from 'notiflix';
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject 
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay }); // Fulfill
+      } else {
+        reject({ position, delay }); // Reject 
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+      const delay = parseInt(formData.get('delay'));
+      const step = parseInt(formData.get('step'));
+      const amount = parseInt(formData.get('amount'));
+
+      for (let i = 0; i < amount; i++) {
+        const currentDelay = delay + (i * step);
+        createPromise(i + 1, currentDelay)
+          .then(({ position, delay }) => {
+            Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          })
+          .catch(({ position, delay }) => {
+             Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          });
+      }
+    });
